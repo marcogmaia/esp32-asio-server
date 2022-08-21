d47fa00d22595effe1c607728537c5124895bfd7
Marco Maia <marco.maia@iarasystems.com.br>
2022-08-21 18:14:09 -0300

    initial commit

---
+++ main/main.cpp
@@ -0,0 +1,46 @@
+#include <exception>
+#include <iostream>
+
+#include <driver/gpio.h>
+#include <esp_log.h>
+#include <nvs_flash.h>
+#include <asio.hpp>
+
+#include "server.h"
+#include "wifi_server.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+constexpr auto kTag = "MAIN";
+
+void InitNvs() {
+  esp_err_t ret = nvs_flash_init();
+  if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+    ESP_ERROR_CHECK(nvs_flash_erase());
+    ret = nvs_flash_init();
+  }
+  ESP_ERROR_CHECK(ret);
+}
+
+void app_main(void) {
+  // Initialize NVS
+  InitNvs();
+
+  ESP_LOGI(kTag, "ESP_WIFI_MODE_STA");
+  WifiInitStation();
+
+  try {
+    asio::io_context io_context;
+    uint16_t port = 54321;
+    Server server(io_context, port);
+    io_context.run();
+  } catch (std::exception& e) {
+    ESP_LOGE(kTag, "Exception: %s", e.what());
+  }
+}
+
+#ifdef __cplusplus
+}
+#endif
---
+++ main/server.cpp
@@ -0,0 +1,71 @@
+//
+// async_tcp_echo_server.cpp
+// ~~~~~~~~~~~~~~~~~~~~~~~~~
+//
+// Copyright (c) 2003-2019 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include <cstdlib>
+#include <exception>
+#include <iostream>
+#include <memory>
+#include <utility>
+
+#include <nlohmann/json.hpp>
+
+#include <asio.hpp>
+
+#include "server.h"
+
+namespace {
+
+using asio::ip::tcp;
+using Json = nlohmann::json;
+
+}  // namespace
+
+void Server::DoAccept() {
+  acceptor_.async_accept(socket_, [this](std::error_code ec) {
+    if (!ec) {
+      std::make_shared<Session>(std::move(socket_))->Start();
+    }
+    DoAccept();
+  });
+}
+
+Server::Server(asio::io_context& io_context, short port)
+    : acceptor_(io_context, tcp::endpoint(tcp::v4(), port)), socket_(io_context) {
+  DoAccept();
+}
+
+void Session::DoRead() {
+  auto self(shared_from_this());
+  socket_.async_read_some(asio::buffer(data_, kMaxLen),
+                          [this, self](std::error_code ec, std::size_t length) {
+                            if (!ec) {
+                              std::string str(data_, length);
+                              Json json;
+                              json["str"] = str;
+                              ESP_LOGI("Asio", "Message received: %s", json.dump(2).c_str());
+                              DoWrite(length);
+                            }
+                          });
+}
+
+void Session::DoWrite(std::size_t length) {
+  auto self(shared_from_this());
+  asio::async_write(socket_,
+                    asio::buffer(data_, length),
+                    [this, self, length](std::error_code ec, std::size_t /*length*/) {
+                      if (!ec) {
+                        std::string message(data_, length);
+                        if (!message.empty()) {
+                          ESP_LOGI("Asio", "Message written: %s", message.c_str());
+                        }
+                        DoRead();
+                      }
+                    });
+}
